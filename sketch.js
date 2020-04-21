12c957cd6151864f915197db0595ffc739764cc3
Ojas Saxena <ojassaxena03@gmail.com>
2020-04-21 23:46:14 +0530

    update

--- sketch.js
+++ sketch.js
@@ -3,16 +3,22 @@ function setup()
     createCanvas(800, 800);
 }
 
+var centres = [];
+
 function draw()
 {
     background(0);
-    xWindow = 800;
-    yWindow = 800;
-    centres = [];
-    xCentre = mouseX;;
-    yCentre = mouseY;
-    centres.push([xCentre , yCentre]);
+    circleSize = 20;
     fill(color(230, 50, 37));
     noStroke();
-    circle(xCentre , yCentre , 30)
+    for(let i=0 ; i<centres.length ; i++)
+        circle(centres[i][0] , centres[i][1] , circleSize);
+    if(mouseIsPressed)
+    {
+        xCentre = round(mouseX);
+        yCentre = round(mouseY);
+        circle(xCentre , yCentre , circleSize);
+        centres.push([xCentre , yCentre]);
+    }
+    print(centres.length);
 }
